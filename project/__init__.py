794aca2a9d82bdc372596b9979189caa660add65
Alexandre <almmello@gmail.com>
2022-02-17 14:39:32 -0300

    new rout added

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_from_directory
 
 
 app = Flask(__name__)
@@ -6,9 +6,15 @@ app = Flask(__name__)
 
 
 @app.route("/")
-
 def home():
     return render_template('home.html')
+
+
+@app.route('/rga/<path:path>')
+def send_js(path):
+    return send_from_directory('rga', path)
+
+
 
 
 if __name__ == '__main__':
